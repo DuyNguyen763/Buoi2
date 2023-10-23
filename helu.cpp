0da3de829bad3cab3ef290a103bd3d96daf9464e
DuyNguyen763 <148051751+DuyNguyen763@users.noreply.github.com>
2023-10-23 16:40:22 +0700

    Update helu.cpp

--- helu.cpp
+++ helu.cpp
@@ -1,3 +1,11 @@
-hello a ji nô mô tô
+#hello a ji nô mô tô
 
-1234554321
+#1234554321
+using namespace std;
+int main()
+{
+  cout << "Hi Duyyy!"
+  cout << "Chào cậu!"
+  system("pause");
+  return 0;
+}
